86fa75a7da58f2309249ac5edd8d11d207f9e18b
H16304013 <fn16304013@ga.tera-house.ac.jp>
2017-01-31 16:19:37 +0900

    for文追加

--- shikoshikomonkey.java
+++ shikoshikomonkey.java
@@ -2,5 +2,8 @@ class ex{
     public static void main(String[] args) {
         System.out.println("‚¤‚ñ‚±ƒvƒƒOƒ‰ƒ€");
         System.out.println("‚«‚½‚È‚¢‚¼");
+        for(int i = 0;i <= 10;i++){
+            System.out.println("‚«‚½‚È‚­‚È‚¢");
+        }
     }
 }
