24372fa4af61b765544813e59321ec39d50bcdc1
BuildTools <unconfigured@null.spigotmc.org>
2017-01-31 16:15:57 +0900

    きたねぇぞ

--- shikoshikomonkey.java
+++ shikoshikomonkey.java
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-class shiko{
-public static void main(String[] args){
-		System.out.println("ã‚ã³ã‚ƒã³ã‚ƒã³ã‚ƒã³ã‚ƒ");
-	}
-}
-=======
-
-
 class ex{
     public static void main(String[] args) {
         System.out.println("‚¤‚ñ‚±ƒvƒƒOƒ‰ƒ€");
+        System.out.println("‚«‚½‚È‚¢‚¼");
     }
 }
->>>>>>> c9950547048fcc438385d1ba58bbd0ff3fc94fce
