e7e41341b16ed054679b492326af1c91999b451d
H16304013 <fn16304013@ga.tera-house.ac.jp>
2017-03-02 12:15:30 +0900

    スレッドからレスページへ転送機能実装

--- project GL/first/WEB-INF/classes/Execute/ResExecute.java
+++ project GL/first/WEB-INF/classes/Execute/ResExecute.java
@@ -9,24 +9,27 @@ public class ResExecute{
 
 	DBAccess dba = new DBAccess();
 
+	String thrid = null;
+
 	public void addRes(ResBean rb){
 		String sql;
 
 		String resUserName = rb.getResUserName();
-		System.out.println("今からレスの挿入するよ"+rb.getThrId());
+		System.out.println("今からレスの挿入するよ"+rb.getThrId()+resUserName);
 		if(resUserName==""){
 			sql="insert into GLRes(thrId,resId,CreateDate,Content) values ('"+rb.getThrId()+"',ressequence.nextval,sysdate,'"+rb.getResContent()+"')";
 		}else{
-			sql="insert into GLRes(thrId,resId,UserName,CreateDate,Content) values ('"+rb.getThrId()+"',ressequence.nextval,'"+resUserName+"',sysdate,'"+rb.getResContent()+"')";
+			sql="insert into GLRes(thrId,resId,UserName,CreateDate,Content) values ('"+thrid+"',ressequence.nextval,'"+resUserName+"',sysdate,'"+rb.getResContent()+"')";
 		}
 		System.out.println("今からレスの挿入したよ");
 
 		dba.write(sql);
 	}
 	public ArrayList getRes(String _id){
-		String id = _id;
+		thrid = _id;
+		System.out.println("ThrIdの確認"+thrid);
 
-		return dba.resRead(id);
+		return dba.resRead(thrid);
 
 	}
 }
